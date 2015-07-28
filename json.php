6f7dcf554372d3c35a9778886f79281e54158051
mainfo <mainfo.cartong@google.com>
2015-07-28 09:00:00 +0200

    Update json.php

--- json.php
+++ json.php
@@ -8,7 +8,7 @@ if (!in_array($action, $actions)) { die("Bad action: $action"); }
 
 error_reporting(-1); // report all errors
 
-$ushahidi_config_db = "/var/www/ushahidi/application/database.php";
+$ushahidi_config_db = "/var/www/ushahidi/application/config/database.php";
 
 define('SYSPATH', "dummy");
 unset($config);
